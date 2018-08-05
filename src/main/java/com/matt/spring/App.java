613cfab5f5ba79823d4dabe3f8c236f132566e33
matt <mathpier305@yahoo.com>
2018-08-05 14:57:58 -0400

    integrate with wildfly ;

--- src/main/java/com/matt/spring/App.java
+++ src/main/java/com/matt/spring/App.java
@@ -2,9 +2,11 @@ package com.matt.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class App {
+public class App extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
